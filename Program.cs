89767d295fb909b83ccfaf8f24cfce8c3f99150b
isablasque <isabelablasque@gmail.com>
2024-10-09 15:05:06 -0300

    commit isa

--- Program.cs
+++ Program.cs
@@ -6,8 +6,8 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
-/*builder.Services.AddDbContext<Contexto> //Blasque
-    (options => options.UseSqlServer("Data Source=SP-1491030\\SQLSENAI;Initial Catalog = 5Letras-Banco;Integrated Security = True;TrustServerCertificate = True")); */
+builder.Services.AddDbContext<Contexto> //Blasque
+    (options => options.UseSqlServer("Data Source=SP-1491030\\SQLSENAI;Initial Catalog = 5Letras-Banco;Integrated Security = True;TrustServerCertificate = True")); 
 
 /*builder.Services.AddDbContext<Contexto> //Gyuliana
     (options => options.UseSqlServer("Data Source=SP-1491016\\SQLSENAI;Initial Catalog = 5Letras-Banco;Integrated Security = True;TrustServerCertificate = True"));*/
@@ -15,8 +15,8 @@ builder.Services.AddControllersWithViews();
 /* builder.Services.AddDbContext<Contexto> //Iara
     (options => options.UseSqlServer("Data Source=SP-1491033\\SQLSENAI;Initial Catalog = 5Letras-Banco;Integrated Security = True;TrustServerCertificate = True")); */
 
-builder.Services.AddDbContext<Contexto> //Renara
-    (options => options.UseSqlServer("Data Source=SP-1491035\\SQLSENAI;Initial Catalog = 5Letras-Banco;Integrated Security = True;TrustServerCertificate = True"));
+/*builder.Services.AddDbContext<Contexto> //Renara
+    (options => options.UseSqlServer("Data Source=SP-1491035\\SQLSENAI;Initial Catalog = 5Letras-Banco;Integrated Security = True;TrustServerCertificate = True")); */
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
