8fa09133476a7ca3f9ecf36f1caaecdf5fc42d05
iaralvr <iaralanzasenai@gmail>
2024-11-13 14:51:01 -0300

    iara

--- Program.cs
+++ Program.cs
@@ -15,8 +15,8 @@ builder.Services.AddControllersWithViews();
 /* builder.Services.AddDbContext<Contexto> //Iara
     (options => options.UseSqlServer("Data Source=SP-1491033\\SQLSENAI;Initial Catalog = 5Letras-Banco;Integrated Security = True;TrustServerCertificate = True")); */
 
-builder.Services.AddDbContext<Contexto> //Renara
-    (options => options.UseSqlServer("Data Source=SP-1491035\\SQLSENAI;Initial Catalog = 5Letras-Banco;Integrated Security = True;TrustServerCertificate = True"));
+/*builder.Services.AddDbContext<Contexto> //Renara
+    (options => options.UseSqlServer("Data Source=SP-1491035\\SQLSENAI;Initial Catalog = 5Letras-Banco;Integrated Security = True;TrustServerCertificate = True")); */
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
