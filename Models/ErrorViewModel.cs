365a98309d0eca4ae1fb1b2f9acbbec182ec43fe
renaraff <refurtado2007@gmail.com>
2024-11-19 11:04:54 -0300

    Merge

--- Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace _5LetrasBanco.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
